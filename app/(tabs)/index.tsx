d994a17b167bf1beb39c10730f7d3c59e7457da1
Rafael Moreira <92860308+raffnb@users.noreply.github.com>
2024-07-14 10:03:16 -0300

    Interface -  FrontEnd
    Criação da interface com ReactNative

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,84 @@
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native'
+import Slider from '@react-native-community/slider'
 
 export default function App (){
     return(
 
-        <View>
-        <Text style = {{marginTop: 34, fontSize: 30}}>Sujeto Programador</Text>
+        <View style = {styles.container}>
+        <Image
+        source={require("@/src/assets/logo.png")}
+        style = {styles.logo}
+        />
+
+        <Text style = {styles.title}>20 Caracteres</Text>
+
+        <View style = {styles.area}>
+            <Slider 
+            style = {{height:50}}
+            minimumValue={6}
+            maximumValue={20}
+            maximumTrackTintColor='#ff0000'
+            minimumTrackTintColor='#000'
+            thumbTintColor='#392de9'
+            />
+        </View>
+
+        <TouchableOpacity style = {styles.button}>
+            <Text style = {styles.buttonText}> Gerar Senha</Text>
+        </TouchableOpacity>
+
+        
         </View>
 
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor: "F3F3FF",
+        justifyContent: 'center',
+        alignItems: 'center'
+        
+    },
+
+    logo:{
+        marginBottom: 60,
+
+    },
+
+    area: {
+        marginBottom: 14,
+        marginTop: 14,
+        width: "80%",
+        backgroundColor: "FFF",
+        borderRadius: 8,
+        padding: 8,
+
+
+    },
+
+    button:{
+        backgroundColor: '#392de9',
+        width: '80%',
+        height: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius:8,
+        marginBottom:18
+            },
+
+
+    buttonText:{
+        color: '#FFF',
+        fontSize: 20,
+
+    },
+
+    title:{
+        fontSize:30,
+        fontWeight: 'bold',
+        
+    }
+
+})
